e363346c6bff8571b2506bda79a2138d34d327f8
Pedro Almazan-Guzman <113930479+Pedro0726@users.noreply.github.com>
2023-02-16 18:40:22 +0000

    files

---
+++ main.py
@@ -0,0 +1,18 @@
+# f = open('test.txt', 'a')
+# f.write("this is the best")
+# # read gives us everything 
+# # readline gives us the first line 
+# # print(f.read())
+# f.close()
+# f = open('employees.txt', 'r')
+# # f.write("we have to fire them")
+# print(f.read())
+# f.close()
+with open("test.txt", 'w') as f:
+  f.write("this mess is over")
+ # for line in f:
+ #   print(line, end '')
+  # f_contents = f.read()
+  # f.write("bfcwyubvycwfebiuvh")
+  # print(f)
+  
